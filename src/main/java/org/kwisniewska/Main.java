16c2d621d5c11ee098bd87c0e351c271311b6443
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-09-09 07:56:53 +0200

    first commit

---
+++ src/main/java/org/kwisniewska/Main.java
@@ -0,0 +1,11 @@
+package org.kwisniewska;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+
+  public static void printBonusDatesBetween(int fromYear, int toYear) {
+
+  }
+}
