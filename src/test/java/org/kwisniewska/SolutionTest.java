89ca386adc0f916b142f79c9e10c36829a275c98
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-09-09 09:22:43 +0200

    Add tests for too small and too large dates

--- src/test/java/org/kwisniewska/SolutionTest.java
+++ src/test/java/org/kwisniewska/SolutionTest.java
@@ -8,7 +8,7 @@ import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut;
 class SolutionTest {
 
   @Test
-  void shouldPrintCorrectDates() throws Exception{
+  void givenCorrectInput_shouldPrintCorrectDates() throws Exception{
     //given
     int fromYear = 2010;
     int toYear = 2015;
@@ -24,10 +24,41 @@ class SolutionTest {
   }
 
   @Test
-  void shouldPrintInvalidData() throws Exception{
+  void givenDatesInWrongOrder_shouldPrintErrorMessage() throws Exception{
     //given
     int fromYear = 2020;
     int toYear = 2015;
+    String expected = "Invalid data";
+
+    //when
+    String text = tapSystemOut(() -> {
+      Solution.printBonusDatesBetween(fromYear, toYear);
+    });
+
+    //then
+    Assert.assertEquals(expected, text.trim());
+  }
+
+  @Test
+  void givenTooSmallDate_shouldPrintErrorMessage() throws Exception{
+    //given
+    int fromYear = 1700;
+    int toYear = 2015;
+    String expected = "Invalid data";
+
+    //when
+    String text = tapSystemOut(() -> {
+      Solution.printBonusDatesBetween(fromYear, toYear);
+    });
+
+    //then
+    Assert.assertEquals(expected, text.trim());
+  }
+  @Test
+  void givenTooLargeDate_shouldPrintErrorMessage() throws Exception{
+    //given
+    int fromYear = 2020;
+    int toYear = 2250;
     String expected = "Invalid data";
 
     //when
